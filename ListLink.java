ac50acd44ad147362d5a5664a38acf019cbd8b3f
vit21513 <vit21513@yandex.ru>
2023-04-02 15:10:10 +0300

    fif3n

--- ListLink.java
+++ ListLink.java
@@ -1,9 +1,13 @@
+
+//Необходимо реализовать метод разворота связного списка (двухсвязного)
+
+
 public class ListLink {
     Node head;
     Node tail;
 
 
-    public void revert() {
+    public void revertListlink() {
 
         Node currentNode = head;
         while (currentNode != null) {
@@ -20,90 +24,85 @@ public class ListLink {
             currentNode = next;
         }
     }
+// не стал иные методы удалять. Описание класса  Node ниже 
+    // public void dell(Node node) {
+    //     Node previous = node.previous;
+    //     Node next = node.next;
+    //     if (previous == null) {
+    //         next.previous = null;
+    //         head = next;
+    //     } else {
+    //         if (next == null) {
+    //             previous.next = null;
+    //             tail = previous;
+    //         } else {
+    //             previous.next = next;
+    //             next.previous = previous;
+    //         }
+    //     }
+    // }
 
+    // public Node find(int value) {
+    //     Node curentNode = head;
+    //     while (curentNode != null) {
+    //         if (curentNode.value == value) {
+    //             return curentNode;
+    //         }
+    //         curentNode = curentNode.next;
+    //     }
+    //     return null;
+    // }
 
+    // public void add(int value) {
+    //     Node node = new Node();
+    //     node.value = value;
+    //     if (head == null) {
+    //         head = node;
+    //         tail = node;
+    //     } else {
+    //         tail.next = node;
+    //         node.previous = tail;
+    //         tail = node;
+    //     }
+    // }
 
+    // public void add(int value, Node node) {
 
-    public void dell(Node node) {
-        Node previous = node.previous;
-        Node next = node.next;
-        if (previous == null) {
-            next.previous = null;
-            head = next;
-        } else {
-            if (next == null) {
-                previous.next = null;
-                tail = previous;
-            } else {
-                previous.next = next;
-                next.previous = previous;
-            }
-        }
-    }
-
-    public Node find(int value) {
-        Node curentNode = head;
-        while (curentNode != null) {
-            if (curentNode.value == value) {
-                return curentNode;
-            }
-            curentNode = curentNode.next;
-        }
-        return null;
-    }
-
-    public void add(int value) {
-        Node node = new Node();
-        node.value = value;
-        if (head == null) {
-            head = node;
-            tail = node;
-        } else {
-            tail.next = node;
-            node.previous = tail;
-            tail = node;
-        }
-    }
-
-    public void add(int value, Node node) {
-
-        Node next = node.next;
-        Node newNode = new Node();
-        newNode.value = value;
-        node.next = newNode;
-        newNode.previous = node;
-        if (next == null) {
-            tail = newNode;
-        } else {
-            next.previous = newNode;
-            newNode.next = next;
-        }
-    }
-
-   
-
+    //     Node next = node.next;
+    //     Node newNode = new Node();
+    //     newNode.value = value;
+    //     node.next = newNode;
+    //     newNode.previous = node;
+    //     if (next == null) {
+    //         tail = newNode;
+    //     } else {
+    //         next.previous = newNode;
+    //         newNode.next = next;
+    //     }
+    // }
     public class Node {
 
         int value;
         Node next;
         Node previous;
-
+    
         public Node(int value, Node next, Node previous) {
             value = this.value;
             next = this.next;
             previous = this.previous;
         }
-
+    
         public Node(int value) {
-
+    
             value = this.value;
             next = null;
             previous = null;
         }
-
+    
         public Node() {
-            
+    
         }
     }
+    
 
 }
